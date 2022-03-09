ed4e1a95edbe7528d9a70c8bfa174f47e9d22fd3
Kerrick Staley <kerrick@kerrickstaley.com>
2022-03-08 20:45:56 -0500

    Increase bleed

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ const nail_shank_clearance_mm = 0.1;
 const nail_to_top_dist_mm = 20.0;
 const doc_margin_mm = 10.0;
 // The engraving will "bleed" into the cut-away area by engrave_bleed_mm. This prevents weird artifacts at the boundary.
-const engrave_bleed_mm = 0.8;
+// Acrylic needs this more than plywood does.
+const engrave_bleed_mm = 1.2;
 // We add extra_frame_size_mm to the width and height of the frame. One thing this accounts for is laser kerf. But kerf
 // should only be 0.2 mm, not 1.0 mm. So what is the extra 0.8 mm? I don't know. The picture just always seems to hang
 // off the frame if we don't add this.
